513ac2a2ada1399e3b49e982207d03a44ac0e2b2
john <nergilz@yandex.ru>
2018-06-24 09:39:17 +0200

    func main() added

--- coursera.py
+++ coursera.py
@@ -35,30 +35,16 @@ def get_html_text(link):
 def get_course_info(html):
     soup = BeautifulSoup(html, 'lxml')
 
-    try:
-        name = soup.find('div', class_='course-info bt3-col-xs-6').find('h2', class_='headline-4-text course-title').text.strip()
-    except AttributeError:
-        name = None
-
-    try:
-        commitment = soup.find('td', class_='td-data').text.strip()
-    except AttributeError:
-        commitment = None
+    course_name = soup.find('div', class_='course-info bt3-col-xs-6').find('h2', class_='headline-4-text course-title').text.strip()
+    commitment = soup.find('td', class_='td-data').text.strip()
 
     try:
         rating = soup.find('div', class_='ratings-text headline-2-text').find('span').next.next
     except AttributeError:
         rating = None
-
-    try:
-        language = soup.find('div', class_='rc-Language').next.next
-    except AttributeError:
-        language = None
-
-    try:
-        start_date = soup.find('div', id='start-date-string').find('span').text.strip()
-    except AttributeError:
-        start_date = None
+        
+    language = soup.find('div', class_='rc-Language').next.next
+    start_date = soup.find('div', id='start-date-string').find('span').text.strip()
 
     if rating is not None:
         rating = rating.split()[1]
@@ -66,35 +52,36 @@ def get_course_info(html):
         date = start_date.split()
         start_date = '{}-{}'.format(date[1], date[2])
 
-    return name, rating, language, start_date, commitment
+    return course_name, rating, language, start_date, commitment
 
 
 def output_courses_info_to_xlsx(courses_info):
-    wbook = Workbook()
-    wsheet = wbook.active
+    work_book = Workbook()
+    work_sheet = work_book.active
 
     for row in courses_info:
-        wsheet.append(row)
+        work_sheet.append(row)
 
-    wbook.save('coursera_courses.xlsx')
+    work_book.save('coursera_courses.xlsx')
 
 
-if __name__ == '__main__':
+def main():
     url = 'https://www.coursera.org/sitemap~www~courses.xml'
     courses_slug = []
+    count_urls = 20
 
     try:
         get_file_xml(url)
         courses_list = get_courses_list()
-        courses_slug = random.sample(courses_list, 20)
+        courses_slug = random.sample(courses_list, count_urls)
         columns = ['Id', 'Course_name', 'Rating', 'Language', 'Start_date', 'Commitment', 'Link']
         courses_info = [columns]
 
         for count_id, link in enumerate(courses_slug, 1):
             course_data = [count_id]
-            html = get_html_text(link)
-            info = get_course_info(html)
-            course_data.extend(info)
+            course_html = get_html_text(link)
+            course_info = get_course_info(course_html)
+            course_data.extend(course_info)
             course_data.append(link)
             courses_info.append(course_data)
 
@@ -102,3 +89,7 @@ if __name__ == '__main__':
 
     except requests.HTTPError as error:
         sys.exit('ERROR: {}'.format(error))
+
+
+if __name__ == '__main__':
+    main()
