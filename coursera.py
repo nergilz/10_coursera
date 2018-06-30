6d85f5decd52ec8ff0f16687d295fb16b12212f1
john <nergilz@yandex.ru>
2018-07-01 01:50:00 +0200

    def save_wb() added, rename var

--- coursera.py
+++ coursera.py
@@ -16,25 +16,36 @@ def get_arguments():
         default='coursera-courses.xlsx',
         help='This is name for result .xlsx file'
         )
+    parser.add_argument(
+        '--count',
+        required=False,
+        type=int,
+        default=20,
+        help='Count urls for fetch information about course'
+        )
     return parser.parse_args()
 
 
 def fetch_html(url):
-    response = requests.get(url)
-    response.encoding = 'utf-8'
-    return response.text
+    try:
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+        return response.text
+
+    except requests.HTTPError as error:
+        sys.exit('ERROR: {}'.format(error))
 
 
-def get_courses_list(html_with_links):
-    courses_list = []
+def get_courses_urls(xtml_with_links):
+    courses_urls = []
 
-    soup = BeautifulSoup(html_with_links, 'lxml')
+    soup = BeautifulSoup(xtml_with_links, 'lxml')
     loc_tags = soup.find_all('url')
 
     for element in loc_tags:
-        courses_list.append(element.find('loc').next)
+        courses_urls.append(element.find('loc').next)
 
-    return courses_list
+    return courses_urls
 
 
 def get_course_info(course_html):
@@ -90,58 +101,49 @@ def get_course_info(course_html):
         }
 
 
-def output_courses_info_to_xlsx(data_for_xlsx):
+def get_courses_data(random_urls):
+    courses_data = {}
+
+    for link in random_urls:
+        course_html = fetch_html(link)
+        course_info = get_course_info(course_html)
+        courses_data.update({link: course_info})
+
+    return courses_data
+
+
+def output_courses_info_to_xlsx(courses_data):
+    columns = ('Course_name', 'Rating', 'Language', 'Start_date', 'Commitment', 'Link')
     work_book = Workbook()
     work_sheet = work_book.active
+    work_sheet.append(columns)
 
-    for row in data_for_xlsx:
-        work_sheet.append(row)
+    for link, data in courses_data.items():
+        work_sheet.append(
+            (data['course_name'],
+             data['rating'],
+             data['language'],
+             data['start_date'],
+             data['commitment'],
+             link)
+            )
 
     return work_book
 
 
-def main():
-    url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    arguments = get_arguments()
-    courses_slug = []
-    count_urls = 20
+def save_work_book(course_data):
 
-    try:
-        html_with_links = fetch_html(url)
-        courses_list = get_courses_list(html_with_links)
-        courses_slug = random.sample(courses_list, count_urls)
-        columns = [
-            'Id',
-            'Course_name',
-            'Rating',
-            'Language',
-            'Start_date',
-            'Commitment',
-            'Link'
-            ]
-        data_for_xlsx = [columns]
-
-        for count_id, link in enumerate(courses_slug, 1):
-            course_data = [count_id]
-            course_html = fetch_html(link)
-            course_info = get_course_info(course_html)
-
-            course_data.extend(
-                [course_info['course_name'],
-                 course_info['rating'],
-                 course_info['language'],
-                 course_info['start_date'],
-                 course_info['commitment']]
-                )
-            course_data.append(link)
-            data_for_xlsx.append(course_data)
-
-        work_book = output_courses_info_to_xlsx(data_for_xlsx)
-        work_book.save(arguments.output)
-
-    except requests.HTTPError as error:
-        sys.exit('ERROR: {}'.format(error))
+    work_book = output_courses_info_to_xlsx(courses_data)
+    work_book.save(arguments.output)
 
 
 if __name__ == '__main__':
-    main()
+    url_xml_feed = 'https://www.coursera.org/sitemap~www~courses.xml'
+    arguments = get_arguments()
+    random_urls = []
+
+    xml_with_links = fetch_html(url_xml_feed)
+    courses_urls = get_courses_urls(xml_with_links)
+    random_urls = random.sample(courses_urls, arguments.count)
+    courses_data = get_courses_data(random_urls)
+    save_work_book(courses_data)
